3c52c1e466d3fba0bd5c0ba2c101b1fcbe13f1a2
falk <frabatnun97@gmail.com>
2020-01-20 18:45:46 +0100

    Views and some HTML adapted
    Films -> Peripheral functionality

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 from django import forms
 
+
 class UserForm(forms.Form):
     id = forms.CharField(label='User ID')
-    
-class FilmForm(forms.Form):
-    id = forms.CharField(label='Movie ID')
+
+
+class PeripheralForm(forms.Form):
+    id = forms.CharField(label='Peripheral ID')
